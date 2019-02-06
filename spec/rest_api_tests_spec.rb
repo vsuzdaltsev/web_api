7c992b97d2ee7fa29d7ab0810adc5501f4a347a4
Vsevolod Suzdaltsev <vsevolod_suzdaltsev@epam.com>
2019-02-06 20:55:53 +0300

    fix spec descriptions

--- spec/rest_api_tests_spec.rb
+++ spec/rest_api_tests_spec.rb
@@ -8,19 +8,23 @@ def app
 end
 
 describe 'post_create_future_route' do
-  it 'should successfully return default response from the route' do
-    post "#{WebApiConf::DEFAULTS[:api_url_base_v1]}/post_create_future"
-    expect(last_response).to be_ok
-    last_response.body.include? "{\"method_name\":\"post_create_future\",\"future\":\"no future\"}\n"
+  context 'when curl post_create_future' do
+    it 'should successfully return default response from the route' do
+      post "#{WebApiConf::DEFAULTS[:api_url_base_v1]}/post_create_future"
+      expect(last_response).to be_ok
+      last_response.body.include? "{\"method_name\":\"post_create_future\",\"future\":\"no future\"}\n"
+    end
   end
 end
 
 describe 'create_default_route_methods' do
-  it 'should successfully return default response from default route' do
-    WebApiConf::DEFAULTS[:valid_http_methods].each do |method|
-      send(method, "#{WebApiConf::DEFAULTS[:api_url_base_v1]}/*") do
-        expect(last_response).to be_ok
-        last_response.body.include? "{\"help\":\"#{method} method default route\"}\n"
+  context 'when curl different http types' do
+    it 'should successfully return default response from default route with http type' do
+      WebApiConf::DEFAULTS[:valid_http_methods].each do |method|
+        send(method, "#{WebApiConf::DEFAULTS[:api_url_base_v1]}/*") do
+          expect(last_response).to be_ok
+          last_response.body.include? "{\"help\":\"#{method} method default route\"}\n"
+        end
       end
     end
   end
